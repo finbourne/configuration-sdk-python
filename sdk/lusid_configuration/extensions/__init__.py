bb30214898ae7d61d01c06174187a0fbdf66e8ea
Concourse <concourse@finbourne.com>
2023-12-01 11:41:32 +0000

    Updates for v2.0.1

--- sdk/lusid_configuration/extensions/__init__.py
+++ sdk/lusid_configuration/extensions/__init__.py
@@ -1,15 +1,7 @@
-from lusid_configuration.extensions.api_client_builder import build_client
 from lusid_configuration.extensions.api_client_factory import SyncApiClientFactory, ApiClientFactory
 from lusid_configuration.extensions.configuration_loaders import (
     ConfigurationLoader,
     SecretsFileConfigurationLoader,
     EnvironmentVariablesConfigurationLoader,
     ArgsConfigurationLoader,
-    get_api_configuration,
 )
-from lusid_configuration.extensions.api_configuration import ApiConfiguration
-from lusid_configuration.extensions.retry import RetryingRestWrapper, RetryingRestWrapperAsync
-from lusid_configuration.extensions.proxy_config import ProxyConfig
-from lusid_configuration.extensions.refreshing_token import RefreshingToken
-from lusid_configuration.extensions.api_client import SyncApiClient
-from lusid_configuration.extensions.rest import RESTClientObject
